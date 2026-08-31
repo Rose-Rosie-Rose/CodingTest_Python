0d2ca0972d70a1bfeca3c0e906a3b576ab00c13a
Rosie <88028208+Rose-Rosie-Rose@users.noreply.github.com>
2026-08-31 17:26:00 +0900

    python basic 12 - 정수 2개 입력받아 그대로 출력하기 1

---
+++ pythonBasic12.py
@@ -0,0 +1,21 @@
+"""
+정수 2개 입력받아 그대로 출력하기1
+
+줄을 바꿔 정수(integer) 2개를 입력받아 줄을 바꿔 출력해보자.
+
+예시
+a = input() 
+b = input()
+a=int(a)
+b=int(b)
+print(a)
+print(b)
+과 같은 방법으로 가능하다.
+
+출력
+입력된 두 정수를 줄을 바꿔 출력한다.
+"""
+a = input()
+b = input() 
+print(a)
+print(b)
