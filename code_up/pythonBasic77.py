3560954a986b61c94c181e7e177db27e0d7a7d42
Rosie <88028208+Rose-Rosie-Rose@users.noreply.github.com>
2026-09-04 09:10:27 +0900

    python basic 77 - 짝수 합 구하기

---
+++ code_up/pythonBasic77.py
@@ -0,0 +1,21 @@
+"""
+짝수 합 구하기
+
+정수(1 ~ 100) 1개를 입력받아 1부터 그 수까지 짝수의 합을 구해보자.
+
+예시
+#다음 코드는 홀 수만 더해 출력한다.
+n = int(input())
+s = 0
+for i in range(1, n+1) :
+  if i%2==1 :
+    s += i
+
+print(s)
+"""
+num = int(input())
+total = 0
+
+for i in range(1, num + 1): 
+    if i % 2 == 0: total += i
+print(total)
